c54ea30e66ad076e4360e799db66184765a23b20
stniyaz <bd7vecazg@code.edu.az>
2025-05-22 23:31:46 +0400

    adminTemplateSplitIntoLayoutAndDashboardPageReady

---
+++ EcommerceApp/src/Frontends/EcommerceApp.WebUI/Areas/Manage/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceApp.WebUI.Areas.Manage.Controllers;
+
+[Area("manage")]
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- EcommerceApp/src/IdentityServer/EcommerceApp.IdentityServer/obj/Debug/netcoreapp3.1/EcommerceApp.IdentityServer.AssemblyInfo.cs
+++ EcommerceApp/src/IdentityServer/EcommerceApp.IdentityServer/obj/Debug/netcoreapp3.1/EcommerceApp.IdentityServer.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceApp.IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e18182b1797e34fe5bfcb31fcb611c458bb54bca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a25f01b613e7cf162819692f339834cb5f9f9894")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceApp.IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceApp.IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
